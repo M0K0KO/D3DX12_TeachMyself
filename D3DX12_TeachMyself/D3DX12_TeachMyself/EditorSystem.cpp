352479501ab18528ee209aa53477d2332265997d
박서진 <kevin8193@naver.com>
2026-04-28 23:46:35 +0900

    Show all renderer passes in GPU profiler with culled/unused states

--- D3DX12_TeachMyself/D3DX12_TeachMyself/EditorSystem.cpp
+++ D3DX12_TeachMyself/D3DX12_TeachMyself/EditorSystem.cpp
@@ -697,12 +697,26 @@ void EditorSystem::DrawProfilerPanel()
 {
 	if (ImGui::Begin("GPU Profiler"))
 	{
-		const auto results = m_device->GetGPUProfiler()->GetLastFrameResults();
 		float total = 0.0f;
-		for (const auto& r : results)
+		for (const auto& pass : m_renderer->GetLastFramePassProfilerEntries())
 		{
-			ImGui::Text("%-20s %6.3f ms", r.name.c_str(), r.ms);
-			total += r.ms;
+			switch (pass.state)
+			{
+			case RenderPassRuntimeState::Timed:
+				ImGui::Text("%-32s %6.3f ms", pass.name.c_str(), pass.ms);
+				total += pass.ms;
+				break;
+			case RenderPassRuntimeState::Culled:
+				ImGui::Text("%-32s %s", pass.name.c_str(), "culled");
+				break;
+			case RenderPassRuntimeState::Unused:
+				ImGui::Text("%-32s %s", pass.name.c_str(), "unused");
+				break;
+			case RenderPassRuntimeState::Active:
+			default:
+				ImGui::Text("%-32s %s", pass.name.c_str(), "active");
+				break;
+			}
 		}
 		ImGui::Separator();
 		ImGui::Text("Total: %6.3f ms", total);
@@ -973,4 +987,3 @@ void EditorSystem::HandleFileOpen(EntityScene& scene, std::filesystem::path path
 		MOKOLOG_ERROR("Unknown exception while loading model [{}]", path.string());
 	}
 }
-
